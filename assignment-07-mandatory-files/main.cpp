a9eaf4b87472d63d1be89ecc88e27f33fef034ef
Kai Yuan <146408442+haruhimasamune@users.noreply.github.com>
2023-10-20 21:44:18 +0200

    win1020

--- assignment-07-mandatory-files/main.cpp
+++ assignment-07-mandatory-files/main.cpp
@@ -35,7 +35,7 @@ int enter_command (string filename, string content [MAX_NO_OF_WORDS])
     }    
     else
     {
-        int no_of_words = 0; // array index of content
+        int no_of_words = 0; // index of array `content` 
         string next_word;
         while (read_word (file, next_word) == true && no_of_words < MAX_NO_OF_WORDS) // When there are still words left in file and index doesn't reach the maximum
         {
@@ -43,7 +43,7 @@ int enter_command (string filename, string content [MAX_NO_OF_WORDS])
             no_of_words++;
         }
     file.close();
-    return no_of_words++;
+    return no_of_words;
     }
 }
 
@@ -61,15 +61,14 @@ int count_command (string content [MAX_NO_OF_WORDS], int no_of_words, string ter
     {
         if (content[i] == terms[word_in_term]) // index where the word same as the first word in term
         {
-            int j = i + 1;
-            int k = i + no_of_terms -1;
-            word_in_term++;
-            while (j < k && content[j] == terms[word_in_term])
+            int j = i;
+            int k = i + no_of_terms -1; // index of the last word in term starting at index i
+            while (j < k && content[j] == terms[word_in_term]) // check if the current word in content is the same as the word in term with corresponding index
             {
                 j++;
                 word_in_term++;
             }
-            if (j == k) // we find a term in ieration i
+            if (j == k) // we find a term
             {
             count++;
             }
@@ -79,30 +78,51 @@ int count_command (string content [MAX_NO_OF_WORDS], int no_of_words, string ter
     return count;
 }
 
-int where_command (string content [MAX_NO_OF_WORDS], int no_of_words, string terms [MAX_NO_OF_TERMS], int no_of_terms)
+int where_command (int term_order, string content [MAX_NO_OF_WORDS], int no_of_words, string terms [MAX_NO_OF_TERMS], int no_of_terms)
 {// precondition:
+    assert(term_order >= 0 && term_order < MAX_NO_OF_TERMS);
     assert(no_of_terms >= 0 && no_of_terms <= MAX_NO_OF_TERMS);
     assert(no_of_words >= 0 && no_of_words <= MAX_NO_OF_WORDS);
 /*  postcondition:
-    result is the number of occurrences of the word sequence `terms` (which has `no_of_terms` elements) in `content` (which has `no_of_words` elements)
-    (the result is obviously zero in case `no_of_words` < `no_of_terms`)
+    result is the starting index of occurrences of the word sequence `terms` in order `term_order`
 */
     
-    // implement this function
-    return 0;
+    int count = 0;
+    int word_in_term = 0;
+    int term_index[MAX_NO_OF_TERMS];
+    for (int i = 0; i < no_of_words; i++)
+    {
+        if (content[i] == terms[word_in_term]) // index where the word same as the first word in term
+        {
+            int j = i;
+            int k = i + no_of_terms -1; // index of the last word in term starting at index i
+            while (j < k && content[j] == terms[word_in_term]) // check if the current word in content is the same as the word in term with corresponding index
+            {
+                j++;
+                word_in_term++;
+            }
+            if (j == k) // we find a term in ieration i
+            {
+            term_index[count]=i;        
+            count++;
+            }
+            word_in_term = 0;
+        }
+    }
+    return term_index[term_order] + 1; // starts from 1 not 0
 }
 
-int context_command (int m, string content [MAX_NO_OF_WORDS], int no_of_words, string terms [MAX_NO_OF_TERMS], int no_of_terms)
+int context_command (int con_length, int term_order, string content [MAX_NO_OF_WORDS], int no_of_words, string terms [MAX_NO_OF_TERMS], int no_of_terms)
 {// precondition:
-    m >= 1;
+    assert(con_length >= 1);
     assert(no_of_terms >= 0 && no_of_terms <= MAX_NO_OF_TERMS);
     assert(no_of_words >= 0 && no_of_words <= MAX_NO_OF_WORDS);
 /*  postcondition:
-    result is the number of occurrences of the word sequence `terms` (which has `no_of_terms` elements) in `content` (which has `no_of_words` elements)
-    (the result is obviously zero in case `no_of_words` < `no_of_terms`)
+    If both the above text and below text has the exact same length as `con_length`, return 0
+    If only the above text has length shorter than `con_length`, return 1
+    If only the below text has length shorter than `con_length`, return 2
+    If only the below text has length shorter than `con_length`, return 3
 */
-    
-    // implement this function
     return 0;
 }
 
@@ -113,6 +133,48 @@ int main ()
 /*  postcondition:
     program has presented interactive interface to user, allowing commands to be entered and executed
 */
+    string filename;
+    string content [MAX_NO_OF_WORDS];
+    cout << "Enter the filename: ";
+    cin >> filename;
+    int no_of_words = enter_command(filename, content);
+    if (no_of_words != 0)
+    {
+        cout << "Enter your term: ";
+        int no_of_terms;
+        string terms[MAX_NO_OF_TERMS];
+        for(no_of_terms = 0; no_of_terms < MAX_NO_OF_WORDS; ++no_of_terms)
+        {
+            cin >> terms[no_of_terms];
+        }
+        cout << "You entered term: ";
+        for(int i = 0; i < no_of_terms; ++i)
+        {
+            cout << terms[i] << " ";
+        }
+        int choice;
+        cout << "What command do you want? count-1, where-2, context-3: ";
+        cin >> choice;
+        switch (choice)
+        {
+            case 1:               
+                cout << "Found sequence " << count_command (content, no_of_words, terms, no_of_terms) <<" times in " << no_of_words <<"words";
+                break;
+            case 2:
+                for(int term_order = 0; term_order < count_command (content, no_of_words, terms, no_of_terms); ++term_order)
+                {
+                    cout << "Found occurance at index " << where_command (term_order,content, no_of_words, terms, no_of_terms);
+                }
+                break;
+            case 3:
+
+                break;
+            default:
+                cout << "Invalid command, please type 1,2 or 3.";
+                break;
+        }
+    }    
+
     return 0;
 }
 #endif
